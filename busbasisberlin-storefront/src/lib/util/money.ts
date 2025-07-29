c860b5b8aa5f1068cb054ffa7df9180e2ccf48b4
Marlin Jai <marlin.pohl@on-running.com>
2025-07-29 21:07:32 +0200

    Properly implements dark mode and order email template.

--- busbasisberlin-storefront/src/lib/util/money.ts
+++ busbasisberlin-storefront/src/lib/util/money.ts
@@ -1,26 +1,31 @@
-import { isEmpty } from "./isEmpty"
+import { isEmpty } from './isEmpty';
 
 type ConvertToLocaleParams = {
-  amount: number
-  currency_code: string
-  minimumFractionDigits?: number
-  maximumFractionDigits?: number
-  locale?: string
-}
+	amount: number;
+	currency_code: string;
+	minimumFractionDigits?: number;
+	maximumFractionDigits?: number;
+	locale?: string;
+};
 
 export const convertToLocale = ({
-  amount,
-  currency_code,
-  minimumFractionDigits,
-  maximumFractionDigits,
-  locale = "en-US",
+	amount,
+	currency_code,
+	minimumFractionDigits,
+	maximumFractionDigits,
+	locale = 'en-US',
 }: ConvertToLocaleParams) => {
-  return currency_code && !isEmpty(currency_code)
-    ? new Intl.NumberFormat(locale, {
-        style: "currency",
-        currency: currency_code,
-        minimumFractionDigits,
-        maximumFractionDigits,
-      }).format(amount)
-    : amount.toString()
-}
+	// Handle invalid amount values
+	if (typeof amount !== 'number' || isNaN(amount)) {
+		return '-';
+	}
+
+	return currency_code && !isEmpty(currency_code)
+		? new Intl.NumberFormat(locale, {
+				style: 'currency',
+				currency: currency_code,
+				minimumFractionDigits,
+				maximumFractionDigits,
+		  }).format(amount)
+		: amount.toString();
+};
