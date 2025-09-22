f8d5e51871752ff9d03bfc7d2cc7789070bb85c2
Marlin Jai <marlin.pohl@on-running.com>
2025-09-22 12:12:19 +0200

    Remove unused imports from disabled manual customer link
    The imports are not needed when the link is disabled.
    This should help prevent any residual link-related errors.

--- busbasisberlin/src/links/manual-customer-core-customer.ts
+++ busbasisberlin/src/links/manual-customer-core-customer.ts
@@ -3,9 +3,6 @@
  * Module link between manual customer and core customer
  * TEMPORARILY DISABLED - database relation missing after import
  */
-import { defineLink } from '@medusajs/framework/utils';
-import CustomerModule from '@medusajs/medusa/customer';
-import ManualCustomerModule from '../modules/manual-customer';
 
 // Temporarily disabled - missing link table after database import
 // TODO: Create missing link table or fix linkable definition
