a5566f1d8e796448914e0fe677274de45d469fdb
Marlin Jai <marlin.pohl@on-running.com>
2025-09-22 11:27:34 +0200

    Temporarily disable manual customer link to fix deployment
    The manual customer link was causing database relation errors:
    'relation manualcustomer_manual_customer_customer_customer does not exist'
    
    This was preventing containers from starting properly. Temporarily
    disabled the link to get deployment working. Will fix linkable
    definition and re-enable later.

--- busbasisberlin/src/links/manual-customer-core-customer.ts
+++ busbasisberlin/src/links/manual-customer-core-customer.ts
@@ -1,12 +1,17 @@
 /**
  * manual-customer-core-customer.ts
  * Module link between manual customer and core customer
+ * TEMPORARILY DISABLED - causing deployment issues
  */
 import { defineLink } from '@medusajs/framework/utils';
 import CustomerModule from '@medusajs/medusa/customer';
 import ManualCustomerModule from '../modules/manual-customer';
 
+// Temporarily disabled to fix deployment
+// TODO: Fix linkable definition and re-enable
+/*
 export default defineLink(
 	ManualCustomerModule.linkable.manualCustomer,
 	CustomerModule.linkable.customer,
 );
+*/
