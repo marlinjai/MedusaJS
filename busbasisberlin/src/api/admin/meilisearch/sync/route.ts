d11334b30ba12fe99c14be0eb2e0416b58fae957
Marlin Jai <marlin.pohl@on-running.com>
2025-10-12 21:39:06 +0200

    feat: Complete Meilisearch integration with workflows, subscribers, and admin UI following official docs

---
+++ busbasisberlin/src/api/admin/meilisearch/sync/route.ts
@@ -0,0 +1,16 @@
+// src/api/admin/meilisearch/sync/route.ts
+import { MedusaRequest, MedusaResponse } from '@medusajs/framework/http';
+import { Modules } from '@medusajs/framework/utils';
+
+export async function POST(req: MedusaRequest, res: MedusaResponse) {
+	const eventModuleService = req.scope.resolve(Modules.EVENT_BUS);
+	
+	await eventModuleService.emit({
+		name: 'meilisearch.sync',
+		data: {},
+	});
+	
+	res.send({
+		message: 'Syncing data to Meilisearch',
+	});
+}
---
+++ busbasisberlin/src/workflows/delete-products-from-meilisearch.ts
@@ -0,0 +1,14 @@
+// src/workflows/delete-products-from-meilisearch.ts
+import { createWorkflow } from '@medusajs/framework/workflows-sdk';
+import { deleteProductsFromMeilisearchStep } from './steps/delete-products-from-meilisearch';
+
+type DeleteProductsFromMeilisearchWorkflowInput = {
+	ids: string[];
+};
+
+export const deleteProductsFromMeilisearchWorkflow = createWorkflow(
+	'delete-products-from-meilisearch',
+	(input: DeleteProductsFromMeilisearchWorkflowInput) => {
+		deleteProductsFromMeilisearchStep(input);
+	},
+);
