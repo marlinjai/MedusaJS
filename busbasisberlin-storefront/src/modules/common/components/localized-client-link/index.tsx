7eeceeac5947224614c3655b43d74670b2e8ac27
Marlin Jai <marlin.pohl@on-running.com>
2025-11-18 13:20:06 +0100

    refactor(localized-client-link): standardize code formatting and improve type safety
    - Updated the LocalizedClientLink component to use consistent single quotes for strings.
    - Enhanced type safety by explicitly defining the type of `countryCode` retrieved from useParams.
    - Improved code readability by aligning the component's props and return statement formatting.

--- busbasisberlin-storefront/src/modules/common/components/localized-client-link/index.tsx
+++ busbasisberlin-storefront/src/modules/common/components/localized-client-link/index.tsx
@@ -1,32 +1,32 @@
-"use client"
+'use client';
 
-import Link from "next/link"
-import { useParams } from "next/navigation"
-import React from "react"
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import React from 'react';
 
 /**
  * Use this component to create a Next.js `<Link />` that persists the current country code in the url,
  * without having to explicitly pass it as a prop.
  */
 const LocalizedClientLink = ({
-  children,
-  href,
-  ...props
+	children,
+	href,
+	...props
 }: {
-  children?: React.ReactNode
-  href: string
-  className?: string
-  onClick?: () => void
-  passHref?: true
-  [x: string]: any
+	children?: React.ReactNode;
+	href: string;
+	className?: string;
+	onClick?: () => void;
+	passHref?: true;
+	[x: string]: any;
 }) => {
-  const { countryCode } = useParams()
+	const { countryCode } = useParams() as { countryCode: string };
 
-  return (
-    <Link href={`/${countryCode}${href}`} {...props}>
-      {children}
-    </Link>
-  )
-}
+	return (
+		<Link href={`/${countryCode}${href}`} {...props}>
+			{children}
+		</Link>
+	);
+};
 
-export default LocalizedClientLink
+export default LocalizedClientLink;
